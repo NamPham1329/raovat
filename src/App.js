bae24c0f6324b3c773d9af5ef638825a48a39b2d
NamPham <vannam1329@gmail.com>
2021-04-23 17:01:07 +0700

    sap xep lai cau truc file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from './components/Header';
+import Header from './common/Header';
 // import Category from './components/Category';
 import Product from './components/Product';
-import Footer from './components/Footer';
+import Footer from './common/Footer';
 import Login from './components/Login';
 import Signin from './components/Signin';
+import List from './components/List';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from "react-router-dom"; 
+import Category from './components/Category';
 
 
 class App extends Component {
@@ -22,8 +24,10 @@ class App extends Component {
             <div className="container">
               <Header/>
               <Route exact path="/" component={Product} />
+              <Route exact path="/category" component={Category} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/signin" component={Signin} />
+              <Route exact path="/list" component={List} />
             </div>
             <Footer/>
         </div>
--- src/common/Header/index.jsx
+++ src/common/Header/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from '../logo.png';
-import './css/header.css';
+import logo from './logo.png';
+import './style.css';
 import {
   BrowserRouter as Router,
   Switch,
